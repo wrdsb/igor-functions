fabb998934bedd95e8254b255693f9e5d03ab81a
James Schumann <jschumann@gmail.com>
2017-06-16 10:06:26 -0400

    testing auth

--- create_short_url/index.js
+++ create_short_url/index.js
@@ -1,22 +1,31 @@
 module.exports = function (context, message) {
-    var google = require('googleapis');
-    var googleAuth = require('google-auth-library');
-    var urlshortener = google.urlshortener('v1');
+  var google = require('googleapis');
+  var googleAuth = require('google-auth-library');
+  var urlshortener = google.urlshortener('v1');
 
-    context.log(context);
+  var jwtClient = new google.auth.JWT(
+    process.env['client_email'],
+    null,
+    process.env['private_key'],
+    ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/calendar'], // an array of auth scopes
+    null
+  );
 
-    var params = {
-        shortUrl: 'http://goo.gl/xKbRu3'
-    };
+  context.log(context);
 
-    // get the long url of a shortened url
-    urlshortener.url.get(params, function (err, response) {
-      if (err) {
-          context.log('Encountered error', err);
-      } else {
-          context.log('Long url is', response.longUrl);
-      }
-    });
+  var params = {
+    auth: jwtClient,
+    shortUrl: 'http://goo.gl/xKbRu3'
+  };
 
-    context.done();
+  // get the long url of a shortened url
+  urlshortener.url.get(params, function (err, response) {
+    if (err) {
+      context.log('Encountered error', err);
+    } else {
+      context.log('Long url is', response.longUrl);
+    }
+  });
+
+  context.done();
 };
