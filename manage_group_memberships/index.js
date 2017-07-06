e4be954a1140f366c15316b12f4b0d9bc894dc39
James Schumann <jschumann@gmail.com>
2017-07-06 13:44:54 +0000

    stub out manage_group_memberships

---
+++ manage_group_memberships/index.js
@@ -0,0 +1,8 @@
+module.exports = function (context, message) {
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    context.log(context);
+
+    context.done();
+};
