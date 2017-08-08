bb859bfe79289698aef22d2d290c9a3bc0ab9ecf
James Schumann <jschumann@gmail.com>
2017-08-08 19:09:07 +0000

    working on group_create and group_delete. [wip]

--- group_create/index.js
+++ group_create/index.js
@@ -1,5 +1,80 @@
 module.exports = function (context, message) {
     context.log(message);
 
-    context.done();
+    var waterfall = require('async/waterfall');
+
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var directory = google.admin('directory_v1');
+    var groupssettings = google.groupssettings('v1');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    // *sigh* because Azure Functions application settings can't handle newlines, let's add them ourselves:
+    private_key = private_key.split('\\n').join("\n");
+
+    var group_to_create = message.group;
+    
+    // stores our group in the end
+    var group_created = {};
+
+    // prep our credentials for G Suite APIs
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key, ['https://www.googleapis.com/auth/admin.directory.group','https://www.googleapis.com/auth/apps.groups.settings'], // an array of auth scopes
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+
+        // specify we want JSON back from the API.
+        // Group Settings API defaults to XML (or Atom), despite the docs
+        alt: "json",
+
+        // the Groups resource to create
+        resource: group_to_create,
+        groupUniqueId: group_to_create.email
+    };
+
+    jwtClient.authorize(function(err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        waterfall([
+            function createGroup(createGroupCallback) {
+                directory.groups.insert(params, function (err, result) {
+                    if (err) {
+                        createGroupCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    createGroupCallback(null, result);
+                });
+            },
+            function createGroupSettings(createGroupSettingsCallback) {
+                groupssettings.groups.update(params, function (err, result) {
+                    if (err) {
+                        createGroupSettingsCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    createGroupSettingsCallback(null, result);
+                });
+            }
+        ],
+        function (err, result) {
+            if (err) {
+                context.done(err);
+            } else {
+                context.log(result);
+                context.done();
+            }
+        });
+    });
 };
--- group_delete/index.js
+++ group_delete/index.js
@@ -1,5 +1,54 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
 
-    context.done();
+    var directory = google.admin('directory_v1');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    // *sigh* because Azure Functions application settings can't handle newlines, let's add them ourselves:
+    private_key = private_key.split('\\n').join("\n");
+
+    var group_email = message.group.email;
+    context.log('Delete group: ' + group_email);
+
+    // prep our credentials for G Suite APIs
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key, ['https://www.googleapis.com/auth/admin.directory.group','https://www.googleapis.com/auth/apps.groups.settings'], // an array of auth scopes
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+        
+        // used by Groups (Directory) API 'GET'
+        groupKey: group_email,
+
+        // specify we want JSON back from the API.
+        // Group Settings API defaults to XML (or Atom), despite the docs
+        alt: "json"
+    };
+
+
+    jwtClient.authorize(function(err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        directory.groups.delete(
+            params,
+            function(err, response) {
+                if (err) { 
+                    context.log(err);
+                } else {
+                    context.log(response.statusCode);
+                    context.done();
+                }
+            }
+        );
+    });
 };
