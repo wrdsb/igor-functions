1736ad86abc46b4504b5bd90217b016b5dec759c
James Schumann <jschumann@gmail.com>
2017-08-23 14:36:08 +0000

    test calendar_list_entry_create [wip]

--- calendar_list_entry_create/index.js
+++ calendar_list_entry_create/index.js
@@ -1,5 +1,71 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var series = require('async/series');
 
-    context.done();
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var calendar = google.calendar('v3');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    // *sigh* because Azure Functions application settings can't handle newlines, let's add them ourselves:
+    private_key = private_key.split('\\n').join("\n");
+
+    var calendar_list_entry_params = message.params;
+    var calendar_user = message.user;
+    context.log(calendar_list_entry_params);
+    
+    // stores our calendar list entry in the end
+    var calendar_list_entry_created = {};
+
+    // prep our credentials for G Suite APIs
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/calendar'], // an array of auth scopes
+        calendar_user
+    );
+
+    var params = {
+        auth: jwtClient,
+
+        // specify we want JSON back from the API.
+        // Group Settings API defaults to XML (or Atom), despite the docs
+        alt: "json",
+
+        // the Calendar List Entry to create
+        resource: calendar_list_entry_params.resource,
+        colorRgbFormat: calendar_list_entry_params.colorRgbFormat
+    };
+
+    jwtClient.authorize(function(err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        series([
+            function createCalendarListEntry(createCalendarListEntryCallback) {
+                calendar.calendarList.insert(params, function (err, result) {
+                    if (err) {
+                        createCalendarListEntryCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    createCalendarListEntryCallback(null, result);
+                });
+            }
+        ],
+        function (err, results) {
+            if (err) {
+                context.done(err);
+            } else {
+                calendar_list_entry_created = results[0];
+                context.log(calendar_list_entry_created);
+                context.done();
+            }
+        });
+    });
 };
