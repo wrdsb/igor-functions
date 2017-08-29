e4ad5de80380513f5cb6b18cd3f7da99f2f2e72b
dtwardus <dorian_twardus@wrdsb.ca>
2017-08-29 15:49:19 -0400

    Added feature Calendar Read

--- calendar_read/index.js
+++ calendar_read/index.js
@@ -1,5 +1,61 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var series = require('async/series');
 
-    context.done();
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var calendar = google.calendar('v3');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    private_key = private_key.split('\\n').join("\n");
+
+    var calendar_to_read = message;
+    context.log(calendar_to_read);
+
+    var calendar_read = {};
+
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/calendar'],
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+        alt: "json",
+        calendarId: calendar_to_read
+    };
+
+    jwtClient.authorize(function (err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        series([
+            function readCalendar(readCalendarCallback) {
+                calendar.calendars.get(params, function (err, result) {
+                    if (err) {
+                        readCalendarCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    readCalendarCallback(null, result);
+                });
+            }
+        ],
+            function (err, results) {
+                if (err) {
+                    context.done(err);
+                } else {
+                    calendar_read = results[0];
+                    context.log(calendar_read);
+                    context.done();
+                }
+            });
+    });
 };
