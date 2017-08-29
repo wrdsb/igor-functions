f16363aead6f976b9a2d2fd705eb5b4a91c48187
dtwardus <dorian_twardus@wrdsb.ca>
2017-08-29 15:47:28 -0400

    Added feature Calendar Delete

---
+++ calendar_delete/README.md
@@ -0,0 +1,6 @@
+https://developers.google.com/google-apps/calendar/v3/reference/calendars
+
+https://developers.google.com/google-apps/calendar/v3/reference/calendars/delete
+
+https://github.com/google/google-api-nodejs-client/blob/master/apis/calendar/v3.ts
+Line 638 - "calendar.calendars.delete"
--- calendar_delete/index.js
+++ calendar_delete/index.js
@@ -1,5 +1,58 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var series = require('async/series');
 
-    context.done();
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var calendar = google.calendar('v3');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    private_key = private_key.split('\\n').join("\n");
+
+    var calendar_to_delete = message;
+    context.log(calendar_to_delete);
+
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/calendar'],
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+        alt: "json",
+        calendarId: calendar_to_delete
+    };
+
+    jwtClient.authorize(function (err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        series([
+            function deleteCalendar(deleteCalendarCallback) {
+                calendar.calendars.delete(params, function (err, result) {
+                    if (err) {
+                        deleteCalendarCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    deleteCalendarCallback(null, result);
+                });
+            }
+        ],
+            function (err, results) {
+                if (err) {
+                    context.done(err);
+                } else {
+                    //Success: Empty Response Body
+                    context.done();
+                }
+            });
+    });
 };
