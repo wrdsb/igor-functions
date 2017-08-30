b39ee97f377305af29ad2165d53f5eb3d0e0eab1
dtwardus <dorian_twardus@wrdsb.ca>
2017-08-30 09:52:45 -0400

    Added feature Calender Resource Read

---
+++ calendar_resource_read/README.md
@@ -0,0 +1,6 @@
+https://developers.google.com/admin-sdk/directory/v1/reference/resources/calendars
+
+https://developers.google.com/admin-sdk/directory/v1/reference/resources/calendars/get
+
+https://github.com/google/google-api-nodejs-client/blob/master/apis/admin/directory_v1.ts
+Line 2132 - "directory.resources.calendars.get"
--- calendar_resource_read/index.js
+++ calendar_resource_read/index.js
@@ -1,5 +1,65 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var series = require('async/series');
 
-    context.done();
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var directory = google.admin('directory_v1');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    private_key = private_key.split('\\n').join("\n");
+
+    var calendar_resource_to_read = message.resource;
+    var memberKey = message.member_id;
+    context.log('Read ' + calendar_resource_to_read + ' calendar resource for ' + memberKey);
+
+    var calendar_resource_read = {};
+
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/admin.directory.resource.calendar'],
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+        alt: "json",
+        calendarResourceId: calendar_resource_to_read,
+        customer: memberKey
+    };
+
+    jwtClient.authorize(function (err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        series([
+            function readCalendarResource(readCalendarResourceCallback) {
+                directory.resources.calendars.get(params, function (err, result) {
+                    if (err) {
+                        context.log(result);
+                        readCalendarResourceCallback(new Error(err));
+                        return;
+                    }
+                    context.log(result);
+                    readCalendarResourceCallback(null, result);
+                });
+            }
+        ],
+            function (err, results) {
+                if (err) {
+                    context.done(err);
+                } else {
+                    calendar_resource_read = results[0];
+                    context.log(calendar_resource_read);
+                    context.done();
+                }
+            });
+    });
 };
+
