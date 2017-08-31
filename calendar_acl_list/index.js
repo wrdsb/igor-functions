5e9608531a7fc8d69b41cff1d798be38a218c343
James Schumann <jschumann@gmail.com>
2017-08-31 12:11:18 +0000

    flesh out calendar_acl_list

--- calendar_acl_list/index.js
+++ calendar_acl_list/index.js
@@ -1,5 +1,53 @@
 module.exports = function (context, message) {
-    context.log(message);
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var calendar = google.calendar('v3');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@wrdsb.ca';
+
+    // *sigh* because Azure Functions application settings can't handle newlines, let's add them ourselves:
+    private_key = private_key.split('\\n').join("\n");
+
+    context.log('List Calendar ACLs for calendar ' + message.calendarId);
+
+    // prep our credentials for G Suite APIs
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/calendar'], // an array of auth scopes
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+
+        // specify we want JSON back from the API.
+        // Group Settings API defaults to XML (or Atom), despite the docs
+        alt: "json",
+
+        // the Calendar Acl to create
+        calendarId: message.calendarId,
+        maxResults: message.maxResults
+    };
+
+    jwtClient.authorize(function(err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        calendar.acl.list(params, function (err, result) {
+            if (err) {
+                context.log(result);
+                context.done(err);
+            }
+            context.log(result);
+            context.done();
+        });
+    });
 
     context.done();
 };
