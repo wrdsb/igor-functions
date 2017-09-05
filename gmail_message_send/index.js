00abc4b0f1eceff18abe44daa69b6f07e0c0d2db
dtwardus <dorian_twardus@wrdsb.ca>
2017-09-05 13:38:32 -0400

    Committing test changes

--- gmail_message_send/index.js
+++ gmail_message_send/index.js
@@ -3,6 +3,8 @@ module.exports = function (context, message) {
 
     var google = require('googleapis');
     var googleAuth = require('google-auth-library');
+    //var base64 = require('base64url')
+    var Base64 = require('base64.js').Base64;
 
     var calendar = google.gmail('v1');
 
@@ -13,7 +15,9 @@ module.exports = function (context, message) {
     private_key = private_key.split('\\n').join("\n");
 
     var user_id = message.user_id;
-    //var resource = message.resource;
+    var resource = message.resource;
+    //var base64_encoded_email = base64(resource);
+    //https://github.com/dankogai/js-base64
     //var media = message.media;
     //var media_mime_type = message.media_mime_type;
     //var media_body = message.media_body;
@@ -22,7 +26,7 @@ module.exports = function (context, message) {
     //Params are different to what's listed in
     //https://developers.google.com/gmail/api/v1/reference/users/messages/send
 
-    context.log('Sending email from' + user_id + ' to ');
+    context.log('Sending email from' + user_id + ' to ' );
 
     // stores our calendar in the end
     var user_message_sent = {};
@@ -36,13 +40,14 @@ module.exports = function (context, message) {
         user_address
     );
 
-    //message/rfc822
-    //https://www.npmjs.com/package/internet-message
-    //old example?
-    //https://stackoverflow.com/questions/34546142/gmail-api-for-sending-mails-in-node-js
-    /*
-    function makeBody(to, from, subject, message) {
-        var str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
+    var to = message.to;
+    var from = message.from;
+    var subject = message.subject;
+    var message = message.body;
+    var base64_encoded_email = createEmail(to, from, subject, message);
+
+    function createEmail(to, from, subject, message) {
+        let email = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
             "MIME-Version: 1.0\n",
             "Content-Transfer-Encoding: 7bit\n",
             "to: ", to, "\n",
@@ -51,17 +56,16 @@ module.exports = function (context, message) {
             message
         ].join('');
 
-        var encodedMail = new Buffer(str).toString("base64").replace(/\+/g, '-').replace(/\//g, '_');
-        return encodedMail;
+        return Base64.encodeURI(email);
     }
 
-    var raw = makeBody(user_id, 'james_schumann@wrdsb.ca', 'Function test', 'test message');*/
+    Context.log(base64_encoded_email);
 
     var params = {
         auth: jwtClient,
         alt: "json",
         userId: user_id,
-        resource: resource,
+        resource: {'raw' : base64_encoded_email}
         //media: media
         //media.mimeType: media_mime_type,
         //media.body: media_body
@@ -89,7 +93,7 @@ module.exports = function (context, message) {
                 if (err) {
                     context.done(err);
                 } else {
-                //https://www.npmjs.com/package/rfc822-json
+                //https://www.npmjs.com/package/rfc822-json 
                     user_message_sent = results[0];
                     context.log('Message succesfully sent: ' + user_message_sent);
                     context.done();
