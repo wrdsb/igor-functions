93b1524d4058944e3465ddf8b95073566a457ec8
James Schumann <jschumann@gmail.com>
2017-09-11 14:03:58 +0000

    add group_list

---
+++ group_list/index.js
@@ -0,0 +1,65 @@
+module.exports = function (context, message) {
+    var google = require('googleapis');
+    var googleAuth = require('google-auth-library');
+
+    var directory = google.admin('directory_v1');
+    var groupssettings = google.groupssettings('v1');
+
+    var client_email = process.env.client_email;
+    var private_key = process.env.private_key;
+    var user_address = 'igor@googleapps.wrdsb.ca';
+
+    // *sigh* because Azure Functions application settings can't handle newlines, let's add them ourselves:
+    private_key = private_key.split('\\n').join("\n");
+
+    // prep our credentials for G Suite APIs
+    var jwtClient = new google.auth.JWT(
+        client_email,
+        null,
+        private_key,
+        ['https://www.googleapis.com/auth/admin.directory.group','https://www.googleapis.com/auth/apps.groups.settings'], // an array of auth scopes
+        user_address
+    );
+
+    var params = {
+        auth: jwtClient,
+        alt: "json",
+        customer: process.env.CUSTOMER_ID,
+        maxResults: 200
+    };
+
+    // stores our Groups in the end
+    var groups = {};
+
+    jwtClient.authorize(function(err, tokens) {
+        if (err) {
+            context.log(err);
+            return;
+        }
+        getGroups(params, function() {
+           context.log('Final results: Got ' + Object.getOwnPropertyNames(groups).length + ' groups.');
+           //context.bindings.resultBlob = JSON.stringify(groups);
+           context.done();
+        });
+    });
+
+    function getGroups(params, callback) {
+        directory.groups.list(params, function(err, result) {
+            if (err) {
+                context.log(result);
+                context.done(err);
+            }
+            context.log(result);
+            //context.log('Got ' + result.members.length + ' groups.');
+            //result.members.forEach(function(member) {
+                //members[member.email] = member;
+            //});
+            if (result.nextPageToken) {
+                params.pageToken = result.nextPageToken;
+                getGroups(params, callback);
+            } else {
+                callback();
+            }
+        });
+    }
+};
