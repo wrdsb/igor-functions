fef1e687ede2d3a2a9e475e4e493022a4749c710
James Schumann <jschumann@gmail.com>
2017-09-21 15:18:32 +0000

    connect log_to_slack to slack

--- log_to_slack/index.js
+++ log_to_slack/index.js
@@ -1,5 +1,26 @@
 module.exports = function (context, message) {
-    context.log(context);
+    context.log();
 
+    var request = require('request');
+    var slack_url = process.env.slack_url;
+    var payload = {
+        "username": "IGOR",
+        "text": context.bindings.message
+    };
+        
+    var requestData = {
+        url: slack_url,
+        method: "POST",
+        json: true,
+        headers: {
+            "content-type": "application/json",
+        },
+        body: payload
+    };
+        
+    request(requestData);
+    context.res = {
+        status: 200
+    };
     context.done();
 };
