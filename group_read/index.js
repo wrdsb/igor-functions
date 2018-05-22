e08138d18015b15d67e30ccecaf4f19ffaee7f58
James Schumann <jschumann@gmail.com>
2018-05-22 09:00:57 -0400

    output flynn event and result blob

--- group_read/index.js
+++ group_read/index.js
@@ -1,11 +1,16 @@
 module.exports = function (context, data) {
+    var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+    // Array to store messages being sent to Flynn Grid
+    var events = [];
+
     // TODO: error handling for missing/malformed group email address
-    var group_email = data.group;
-    if (!group_email) {
+    var group_id = data.group;
+    // TODO send error to grid for handling
+    if (!group_id) {
         context.done('Group email missing.');
         return;
     }
-    context.log('Read group: ' + group_email);
+    context.log('Read group: ' + group_id);
     
     var series = require('async/series');
 
@@ -36,11 +41,11 @@ module.exports = function (context, data) {
         auth: jwtClient,
         
         // used by Groups (Directory) API 'GET'
-        groupKey: group_email,
+        groupKey: group_id,
 
         // same as groupKey, but used by Groups Settings (G Suite Admin SDK) API 'GET'
         // because, you know, why standardize the name of our unique identifier?
-        groupUniqueId: group_email,
+        groupUniqueId: group_id,
 
         // specify we want JSON back from the API.
         // Group Settings API defaults to XML (or Atom), despite the docs
@@ -49,6 +54,7 @@ module.exports = function (context, data) {
 
     jwtClient.authorize(function(err, tokens) {
         if (err) {
+            // TODO send error to grid for handling
             context.res = {
                 status: 500,
                 body: err
@@ -82,6 +88,7 @@ module.exports = function (context, data) {
         ],
         function (err, results) {
             if (err) {
+                // TODO send error to grid for handling
                 context.res = {
                     status: 500,
                     body: err
@@ -89,12 +96,49 @@ module.exports = function (context, data) {
                 context.done(err);
                 return;
             } else {
-                group = Object.assign(results[0], results[1]);
+                var message = 'Successfully read group ' + group_id;
+                var event_type = "ca.wrdsb.igor.google_group.read";
+                var flynn_event = {
+                    eventID: `${event_type}-${context.executionContext.invocationId}`,
+                    eventType: event_type,
+                    source: `/google/group/${group_id}`,
+                    schemaURL: "https://mcp.wrdsb.io/schemas/igor/group_read-event.json",
+                    extensions: {
+                        label: "IGOR reads Google Group",
+                        tags: [
+                            "igor",
+                            "google_group",
+                            "google_groups",
+                            "read"
+                        ]
+                    },
+                    data: {
+                        function_name: context.executionContext.functionName,
+                        invocation_id: context.executionContext.invocationId,
+                        payload: {
+                            group: Object.assign(results[0], results[1]),
+                            blobs: [
+                                {
+                                    name: group_id,
+                                    storage_account: "wrdsb-igor_STORAGE",
+                                    path: `groups-groups/${group_id}.json`
+                                }
+                            ]
+                        },
+                        message: message
+                    },
+                    eventTime: execution_timestamp,
+                    eventTypeVersion: "0.1",
+                    cloudEventsVersion: "0.1",
+                    contentType: "application/json"
+                };
+                events.push(JSON.stringify(flynn_event));
                 context.res = {
                     status: 200,
-                    body: group
+                    body: flynn_event.data
                 };
-                context.done(null, JSON.stringify(group));
+                context.log(message);
+                context.done(null, message);
             }
         });
     });
